e90ed84d0c596db2a09992b6806e1dc68f23c034
Santiago Font <santifonlop@hotmail.com>
2022-05-25 05:43:09 -0300

    arreglo de imagenes y descripcion

--- js/js.js
+++ js/js.js
@@ -27,10 +27,8 @@ let guardar = 'https://gateway.marvel.com/v1/public/comics?ts=1&apikey=d6b5cfc2f
             "apikey": "d6b5cfc2f339e698ef1e149331ed4273",
             "ts": "1",
             "hash": "b004e618cd225a9b3e0b713eb3c33e16"   
-        },
-        headers: {
-            'Content-type':'application/json;charset=UTF-8'
         }
+
         })
         const info = await resp.json()
         console.log(info.data.results)
@@ -46,10 +44,11 @@ let guardar = 'https://gateway.marvel.com/v1/public/comics?ts=1&apikey=d6b5cfc2f
             perso.classList.add('personaje')
             perso.innerHTML+=`
                     <p id="nombre">${p.name}</p>
-                    <img id="thumb" src="${p.thumbnail.path}" alt="">
-                    <p id="corto">${p.description}</p>
+                    <img id="thumb" src=${p.thumbnail.path}.${p.thumbnail.extension} alt="">
+                    <p id="corto">${!p.description?"<p id='corto'>Sin descripción</p>":`${p.description}` }</p>
                     <button id="vermas">Ver Más</button>
             `
+     
         persopadre.appendChild(perso)
 
         });
